1e61cd1e397fb84fca94ec344bd131e32138c1a4
harrykellyer57 <147956655+harrykellyer57@users.noreply.github.com>
2025-07-05 07:51:06 +0100

    Update mod.rs

--- kms-connector/crates/gw-listener/src/core/config/mod.rs
+++ kms-connector/crates/gw-listener/src/core/config/mod.rs
@@ -1,8 +1,3 @@
-mod raw;
-pub use parsed::Config as Config;
-mod parsed;
-
-Or, even shorter (assuming the only purpose is to re-export `parsed::Config` and include both modules):
 
 mod raw;
 mod parsed;
